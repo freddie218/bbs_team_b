9b3ce225fa430a5f8d9722e5795ce20139cb2f7c
Phodal <gmszone@qq.com>
2014-01-26 14:08:10 +0800

    [sitemap] Add the package-info.java for xml

---
+++ src/main/java/com/thoughtworks/bbs/web/sitemap/package-info.java
@@ -0,0 +1,8 @@
+@XmlSchema(namespace = "http://www.sitemaps.org/schemas/sitemap/0.9",
+        elementFormDefault = QUALIFIED, xmlns = {@XmlNs(prefix = "",
+        namespaceURI = "http://www.sitemaps.org/schemas/sitemap/0.9")})
+package com.thoughtworks.bbs.web.sitemap;
+
+import static javax.xml.bind.annotation.XmlNsForm.QUALIFIED;
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
